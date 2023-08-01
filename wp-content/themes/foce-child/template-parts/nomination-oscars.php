f4da375ee2834ed3da91f5f3e48ee15075f427e0
Akim <cheikh.akim@gmail.com>
2023-08-01 16:49:36 +0200

    test

--- wp-content/themes/foce-child/template-parts/nomination-oscars.php
+++ wp-content/themes/foce-child/template-parts/nomination-oscars.php
@@ -9,11 +9,7 @@
 
 ?>
 
-<section class="nomination">
-            <div class="nomination__oscars">
-                <p>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</p>
-            </div>
-            <img src="http://localhost/koukaki/wp-content/themes/foce-child/assets/images/18-courts-metrages-francais-d-animation-eligibles-aux-oscars-2021.png" alt="Oscars short film animated">
-
-           
-</section>
+    <div class="nomination__oscars">
+        <p>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</p>
+    </div>
+    <img src="http://localhost/koukaki/wp-content/themes/foce-child/assets/images/18-courts-metrages-francais-d-animation-eligibles-aux-oscars-2021.png" alt="Oscars short film animated">
