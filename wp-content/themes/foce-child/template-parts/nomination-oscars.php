e82b1c11492adc9c0b2fd46b192647e819f585ff
Akim <cheikh.akim@gmail.com>
2023-08-01 13:39:25 +0200

    Ajout des images + creation du template part

---
+++ wp-content/themes/foce-child/template-parts/nomination-oscars.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Fleurs_d\'oranger_&_Chats_errants
+ */
+
+?>
+
+<section class="nomination">
+            <div class="nomination__oscars">
+                <p>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</p>
+            </div>
+            <img src="./assets/images/18-courts-metrages-francais-d-animation-eligibles-aux-oscars-2021.png" alt="Oscars short film animated">
+
+</section>
