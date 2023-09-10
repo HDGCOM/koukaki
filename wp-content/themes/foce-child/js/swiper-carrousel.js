bc390241e7ea693a1592d75c869ce3149243168e
Akim <cheikh.akim@gmail.com>
2023-09-10 15:44:07 +0200

    Modif scss

--- wp-content/themes/foce-child/js/swiper-carrousel.js
+++ wp-content/themes/foce-child/js/swiper-carrousel.js
@@ -1,21 +1,3 @@
-  /*var swiper = new Swiper(".swiper-container", {
-    effect: "coverflow",
-    loop:true,
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "3", 
-    spaceBetween: 30,
-    initialSlide: 0,   
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
-  });*/
-
- 
   var swiper;
 
   function initSwiper() {
